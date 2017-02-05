7f6e8628b00de34954abc0ce2ea7740c6b60f874
shenjiajun <shenjiajun53@qq.com>
2017-02-05 22:10:31 +0800

    for tinkerid

--- app/src/main/java/com/example/jory/learningproject/BottomEditActivity.java
+++ app/src/main/java/com/example/jory/learningproject/BottomEditActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class BottomEditActivity extends AppCompatActivity {
-//sss
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
